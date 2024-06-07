f0a1a2e71703072ff54508947eda90dae724137f
mohamed <mhyahoo2006@gmail.com>
2024-06-07 03:23:41 +0300

    editing something

--- app.js
+++ app.js
@@ -43,8 +43,11 @@ app.use('/sign', sign);
 // routes
 app.get('/', verifyToken, async (req, res, next) => {
     var fields = 0;
-    await field.find({}).select({title: 1, description: 1}).then(v => v.map(v => v._doc)).then(v => fields = v);
-    res.status(200).render('open', { darkMode: req.cookies.mode == "dark", isSigned: Boolean(req.user), fields});
+    await field.find({}).select({title: 1, description: 1}).then(v => v.map(v => v._doc))
+    .then(v => fields = v)
+    .then(v =>{
+        res.status(200).render('open', { darkMode: req.cookies.mode == "dark", isSigned: Boolean(req.user), fields});
+    })
 });
 app.get('/about', (req, res, next) => {
     res.status(200).render('about',

09de5cf5897028e1b06917668f9ee692895955fc
mohamed <mhyahoo2006@gmail.com>
2024-06-08 07:48:14 +0300

    stack

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ const field = require('./models/field');
 const mongLink = process.env.DB_LINKS;
 
 // app setting
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 configureEnv();
 
 // app using
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname + "public"));
 app.use(express.urlencoded());
 app.use(cookieParser());
 
